e44fa219579270c000709ead9f367b238d800519
PEPAYPAGONG <jeffreybacuna5@gmail.com>
2020-01-31 05:19:34 +0800

    Checkout Fragment - Added Tab Layout For Item Category

--- POS ANDROID BACUNA/POS ANDROID BACUNA/Fragments/CheckoutFragment.cs
+++ POS ANDROID BACUNA/POS ANDROID BACUNA/Fragments/CheckoutFragment.cs
@@ -10,17 +10,19 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using static Android.Views.ViewGroup;
 using SupportFragment = Android.Support.V4.App.Fragment;
+using TabLayout = Android.Support.Design.Widget.TabLayout;
 
 namespace POS_ANDROID_BACUNA.Fragments
 {
     public class CheckoutFragment : SupportFragment
     {
+
+        TabLayout mTabs;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -28,7 +30,25 @@ namespace POS_ANDROID_BACUNA.Fragments
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
             View view = inflater.Inflate(Resource.Layout.checkout_fragment, container, false);
+            mTabs = view.FindViewById<TabLayout>(Resource.Id.tabs);
+            string[] categories = { "All", "Polo", "Blouse", "Pants", "Shorts", "Palda", "Caps", "Panyo" };
+            onCreateTabLayout(categories); //add tabs and texts
             return view;
         }
+
+        private void onCreateTabLayout(string[] categories)
+        {
+            for (int i = 0; i < categories.Length; i++)
+            {
+                mTabs.AddTab(mTabs.NewTab().SetText(categories[i]));
+            }
+
+            mTabs.TabSelected += (object sender, TabLayout.TabSelectedEventArgs e) =>
+            {
+                var tab = e.Tab;
+                Android.Widget.Toast.MakeText((MainActivity)this.Activity, "Clicked " + tab.Text + " Tab", Android.Widget.ToastLength.Long).Show();
+            };
+        }
+
     }
 }
